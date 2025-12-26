558f01a0f6a5445349a6cf9dae08a86172dbf53d
Nurbek Izbassar <nurbekizbassar@gmail.com>
2025-12-26 23:23:18 +0500

    fix: db connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,13 @@
 import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle as drizzleBun } from 'drizzle-orm/bun-sql';
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
+import { SQL } from "bun";
 
-const sql = neon(process.env.DATABASE_URL!);
+const isDev = process.env.NODE_ENV === 'development';
 
-export const db = drizzle({ schema, client: sql });
+export const db = isDev
+    ? drizzleNeon({ client: neon(process.env.DATABASE_URL!), schema })
+    : drizzleBun({ client: new SQL(process.env.DATABASE_URL!), schema })
 
 export type Database = typeof db;
